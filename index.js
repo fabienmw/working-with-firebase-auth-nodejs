46462b03e6a4b802c2658ae534b21476efd30a74
Fabien Mwamba <fabien.mwamba@hotmail.fr>
2022-12-02 10:44:30 +0200

    Added list all users

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('welcome to firebase auth demos');
+import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { credential } from "firebase-admin/lib";
+
+initializeApp({
+  credential: credential.cert(credentials),
+});
+
+let totalUsers = 0;
+
+const listAllUsers = (nextPageToken) => {
+  getAuth()
+    .listUsers(1000, nextPageToken)
+    .then((listUsersResult) => {
+      listUsersResult.users.forEach((userRecord) => {
+        totalUsers++;
+        console.log(`user => ${userRecord.toJSON()}`);
+      });
+
+      if (listUsersResult.pageToken) {
+        listAllUsers(listUsersResult.pageToken);
+      }
+    })
+    .catch((error) => {
+      console.error(`Error listing users: ${error}`);
+    });
+};
